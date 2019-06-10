257d3a2d915494dfaac12d61f6191ebbdb4eabe7
ristovkiril <43649881+ristovkiril@users.noreply.github.com>
2019-06-10 17:29:45 +0200

    Adding Menu
    Adding menu buttons on click play button game start, on key press Esc or P pause game

--- Runner/Runner/Form1.Designer.cs
+++ Runner/Runner/Form1.Designer.cs
@@ -29,12 +29,182 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.floor = new System.Windows.Forms.PictureBox();
+            this.floor2 = new System.Windows.Forms.PictureBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // floor
+            // 
+            this.floor.Location = new System.Drawing.Point(-1, 403);
+            this.floor.Name = "floor";
+            this.floor.Size = new System.Drawing.Size(303, 50);
+            this.floor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.floor.TabIndex = 0;
+            this.floor.TabStop = false;
+            // 
+            // floor2
+            // 
+            this.floor2.Location = new System.Drawing.Point(484, 403);
+            this.floor2.Name = "floor2";
+            this.floor2.Size = new System.Drawing.Size(299, 50);
+            this.floor2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.floor2.TabIndex = 1;
+            this.floor2.TabStop = false;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.SeaShell;
+            this.btnPlay.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlay.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnPlay.Location = new System.Drawing.Point(323, 162);
+            this.btnPlay.Margin = new System.Windows.Forms.Padding(5);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(151, 51);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "PLAY";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.BtnPlay_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(570, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Press P or Esc for pause";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.Transparent;
+            this.lblScore.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(82, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(16, 17);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "0";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.SeaShell;
+            this.btnNewGame.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnNewGame.Location = new System.Drawing.Point(287, 225);
+            this.btnNewGame.Margin = new System.Windows.Forms.Padding(5);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(106, 36);
+            this.btnNewGame.TabIndex = 6;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.SeaShell;
+            this.btnSave.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnSave.Location = new System.Drawing.Point(403, 225);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(106, 36);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.SeaShell;
+            this.btnOpen.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpen.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.btnOpen.Location = new System.Drawing.Point(337, 271);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(5);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(127, 39);
+            this.btnOpen.TabIndex = 8;
+            this.btnOpen.Text = "Open Game";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 306);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.floor2);
+            this.Controls.Add(this.floor);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.floor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.floor2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox floor;
+        private System.Windows.Forms.PictureBox floor2;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Runner/Runner/Form1.cs
+++ Runner/Runner/Form1.cs
@@ -16,5 +16,34 @@ namespace Runner
         {
             InitializeComponent();
         }
+        // Menuvanje na kopcinjata za vidlivost
+        public void buttonVisible(bool visible)
+        {
+            btnPlay.Visible = visible;
+            btnNewGame.Visible = visible;
+            btnOpen.Visible = visible;
+            btnSave.Visible = visible;
+        }
+        private void BtnPlay_Click(object sender, EventArgs e)
+        {
+            buttonVisible(false);
+            timer1.Enabled = true;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.P || e.KeyData == Keys.Escape)
+            {
+                timer1.Enabled = false;
+                buttonVisible(true);
+            }
+
+           
+        }
     }
 }
